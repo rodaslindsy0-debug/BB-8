5f0ac7d07b44d731b8bc150ab7f5ee91b63b8709
rodaslindsy0-debug <rodaslindsy0@gmail.com>
2025-11-21 19:50:38 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,9 +2,15 @@
 importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js");
 
-// Configuración de Firebase (solo se usa senderId aquí)
+// Configuración COMPLETA de Firebase (obligatorio)
 firebase.initializeApp({
-  messagingSenderId: "770237885252"
+  apiKey: "AIzaSyDH-Ii_v4nHkBv1-0exU7kvyrcK-vf0SnU",
+  authDomain: "maicol-114de.firebaseapp.com",
+  projectId: "maicol-114de",
+  storageBucket: "maicol-114de.firebasestorage.app",
+  messagingSenderId: "770237885252",
+  appId: "1:770237885252:web:a4d98332eb4c74a4f30511",
+  measurementId: "G-Z8V3X8W8G1"
 });
 
 // Inicializa messaging
@@ -17,7 +23,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification?.title || "Alerta médica";
   const notificationOptions = {
     body: payload.notification?.body || "Tienes una nueva alerta.",
-    icon: "/icon.png" // opcional
+    icon: "/BB-8/icon.png" // opcional
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
